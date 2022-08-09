130576b0c22907ecd166a1f1fbb8cf84f102b385
Zsolt <geczy.zsolt@gmail.com>
2022-08-09 20:00:51 +0200

    Set up logging

--- app/src/main/java/com/gzslt/imagesearch/ImageSearchApplication.kt
+++ app/src/main/java/com/gzslt/imagesearch/ImageSearchApplication.kt
@@ -1,5 +1,14 @@
 package com.gzslt.imagesearch
 
 import android.app.Application
+import logcat.AndroidLogcatLogger
+import logcat.LogPriority
 
-class ImageSearchApplication : Application()
+class ImageSearchApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        AndroidLogcatLogger.installOnDebuggableApp(this, LogPriority.VERBOSE)
+    }
+}
