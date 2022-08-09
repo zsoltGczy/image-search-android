e96b0d1d60a4266e899529c33b15759be894b7a2
Zsolt <geczy.zsolt@gmail.com>
2022-08-09 20:10:04 +0200

    Add dependency injection

--- app/src/main/java/com/gzslt/imagesearch/ImageSearchApplication.kt
+++ app/src/main/java/com/gzslt/imagesearch/ImageSearchApplication.kt
@@ -1,9 +1,11 @@
 package com.gzslt.imagesearch
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import logcat.AndroidLogcatLogger
 import logcat.LogPriority
 
+@HiltAndroidApp
 class ImageSearchApplication : Application() {
 
     override fun onCreate() {
