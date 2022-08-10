ead76ef378787800bc8fc0ba2baaaa6cec131ae8
Zsolt <geczy.zsolt@gmail.com>
2022-08-10 19:31:43 +0200

    Create network module

---
+++ app/src/main/java/com/gzslt/imagesearch/data/api/ApiClient.kt
@@ -0,0 +1,3 @@
+package com.gzslt.imagesearch.data.api
+
+interface ApiClient
---
+++ app/src/main/java/com/gzslt/imagesearch/data/api/NetworkModule.kt
@@ -0,0 +1,58 @@
+package com.gzslt.imagesearch.data.api
+
+import com.gzslt.imagesearch.BuildConfig
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideApiClient(retrofit: Retrofit): ApiClient =
+        retrofit.create(ApiClient::class.java)
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(moshi: Moshi, okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(okHttpClient)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi =
+        Moshi.Builder()
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor() =
+        HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+}
