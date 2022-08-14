72318b511bd92fd4c0b76816145530fa8e115b23
Zsolt <geczy.zsolt@gmail.com>
2022-08-14 21:26:26 +0200

    Create query preferences

--- app/src/main/java/com/gzslt/imagesearch/data/ImageRepository.kt
+++ app/src/main/java/com/gzslt/imagesearch/data/ImageRepository.kt
@@ -7,12 +7,14 @@ import androidx.paging.PagingData
 import com.gzslt.imagesearch.data.api.ApiClient
 import com.gzslt.imagesearch.data.db.AppDatabase
 import com.gzslt.imagesearch.data.db.tuple.ImageListItemTuple
+import com.gzslt.imagesearch.data.preferences.QueryPreferences
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ImageRepository @Inject constructor(
     private val apiClient: ApiClient,
     private val appDatabase: AppDatabase,
+    private val queryPreferences: QueryPreferences,
 ) {
 
     @OptIn(ExperimentalPagingApi::class)
@@ -25,10 +27,14 @@ class ImageRepository @Inject constructor(
             ),
             remoteMediator = ImageRemoteMediator(query, apiClient, appDatabase),
         ) {
+            queryPreferences.saveQuery(query)
             appDatabase.imageDao().getImages()
         }.flow
 
+    fun getSavedQueryOrDefault(): String = queryPreferences.getSavedQuery() ?: DEFAULT_QUERY_VALUE
+
     companion object {
         private const val NETWORK_PAGE_SIZE = 20
+        private const val DEFAULT_QUERY_VALUE = "Dog"
     }
 }
--- app/src/main/java/com/gzslt/imagesearch/main/imagelist/ImageListPresenter.kt
+++ app/src/main/java/com/gzslt/imagesearch/main/imagelist/ImageListPresenter.kt
@@ -20,4 +20,6 @@ class ImageListPresenter @Inject constructor(
                     tuple.toUiModel()
                 }
             }
+
+    fun getSavedQueryOrDefault() = imageRepository.getSavedQueryOrDefault()
 }
